9cce4b2e3acbcb96a015caabb5f799b55f6be06d
skducn <skducn@163.com>
2020-10-15 17:55:23 +0800

    DESKTOP-EOCO1V0_20201015_175523

--- 1casePullByWin764.bat
+++ 1casePullByWin764.bat
@@ -7,4 +7,4 @@ cd\
 ipconfig /all >ip.txt
 for /f "tokens=2 delims=:(Ê×Ñ¡)" %%b in ('findstr /c:"IPv4 µØÖ·" "ip.txt"') do (httpcfg set iplisten -i %%b
 del ip.txt
-"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" %%b))
+"C:\Program Files\Google\Chrome\Application\chrome.exe" %%b:88))
