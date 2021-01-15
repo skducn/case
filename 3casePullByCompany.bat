9bfd179b240392327ec1957b603a1e512c8e5224
skducn <skducn@163.com>
2021-01-15 17:28:18 +0800

    DESKTOP-DO7AEPU_20210115_7:2818

--- 3casePullByCompany.bat
+++ 3casePullByCompany.bat
@@ -1,10 +1,15 @@
 @echo off
 
 git stash
+
 git pull origin master
-d:
-cd\
+
 ipconfig /all >ip.txt
+
 for /f "tokens=2 delims=:(Ê×Ñ¡)" %%b in ('findstr /c:"IPv4 µØÖ·" "ip.txt"') do (httpcfg set iplisten -i %%b
-del ip.txt
-"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" %%b:88))
+
+"C:\Program Files\Google\Chrome\Application\chrome.exe" %%b
+:88)
+)
+
+pause
